869c56d87f156e91b85c87a1c5da43fb621dd46f
Hasibul Hasan Rokib <134086135+HasibulHasanRokib@users.noreply.github.com>
2023-07-12 20:32:56 +0600

    Initial

--- src/Components/WeatherInfo.jsx
+++ src/Components/WeatherInfo.jsx
@@ -43,37 +43,54 @@ useEffect(()=>{
        <div className="weather-icon">
        <img src={weatherImage} alt="" srcSet="" />
        </div>
-       <div className="container">
-        <div className="row weather-info">
-            <div className="col-md-3 degree">
-            <h3 className="degree-text">{temp}&deg;</h3>
+       <div className="">
+        <div className="weather-info">
+            <div className="degree">
+            <h3 className="degree-text">{temp}&deg;C</h3>
             </div>
-            <div className="col-md-3 weather-location">
+            <div className="weather-location">
                 <h5>{weatherMood}</h5>
                 <p>{name},{country}</p>
             </div>
-            <div className="col-md-6 col-sm-12 current-date">
+            {/* <div className="col-md-6 col-sm-12 current-date">
                 {new Date().toLocaleString()}
-            </div>
+            </div> */}
         </div>
        </div>
 
        <div className="weather-more-info">
        <div className="more-info">
+       <div >
+        <img className="info-img" src="/src/assets/sunrise.png" alt="" srcSet="" />
+       </div>
+        <div>
         <span>{timeIs}AM</span>
         <p>Sunrise</p>
+        </div>
        </div>
        <div className="more-info">
-        <samp>{humidity}</samp>
+       <img className="info-img" src="/src/assets/humidity.png" alt="" srcSet="" />
+<div>
+<samp>{humidity}</samp>
         <p>Humidity</p>
+</div>
+       
        </div>
        <div className="more-info">
-        <samp>Pressure</samp>
+       <img className="info-img" src="/src/assets/fresh-air.png" alt="" srcSet="" />
+<div>
+<samp>Pressure</samp>
         <p>{pressure}</p>
+</div>
+        
        </div>
        <div className="more-info">
-        <samp>Wind</samp>
+       <img className="info-img" src="/src/assets/wind-direction.png" alt="" srcSet="" />
+
+       <div>
+       <samp>Wind</samp>
         <p>{speed}</p>
+       </div>
        </div>
 
        </div>
